1dba07f8ec46b2be1f9117476000e141880cbdd4
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2024-12-12 21:22:55 +0800

    initial commit

---
+++ get-angular-dist.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+cp -rv /d/Vincent\ Files/school/cmsc205-project/ui/cmsc205-frontend/dist/cmsc205-frontend .
---
+++ index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require("express");
+
+let app = express();
+
+
+const distDir = '../ui/cmsc205-frontend/dist/cmsc205-frontend/browser/'
+app.use(express.static(path.join(__dirname, distDir)));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, distDir + 'index.html'))
+});
+
+app.listen(4200, () => {
+    console.log(
+        `UI server is running on http://localhost:4200`,
+      );
+})
