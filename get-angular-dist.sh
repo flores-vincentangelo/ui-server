300cf957257bf9e1312964f02db51553b16e6c52
Vincent Flores <94419368+flores-vincentangelo@users.noreply.github.com>
2024-12-17 21:49:11 +0800

    updates to dist

--- get-angular-dist.sh
+++ get-angular-dist.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
+cd /d/Vincent\ Files/school/cmsc205-project/ui/cmsc205-frontend
+rm -rf dist
+npm run build
+cd /d/Vincent\ Files/school/cmsc205-project/ui-server/
 rm -rf cmsc205-frontend
 cp -r /d/Vincent\ Files/school/cmsc205-project/ui/cmsc205-frontend/dist/cmsc205-frontend .
